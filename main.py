ef14452998cfccd6415b748080bee6c073bcf618
Enzolp2 <enzolp2@hotmail.com>
2024-03-27 16:57:12 -0300

    graph testing file

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def main():
   V = [(0, "A"), (1, "B"), (2, "C"), (3, "D"), (4, "E")]
   E = [(0, 1), (1, 2), (2, 3), (3, 0), (3, 4), (0, 4)]
   w = { (0, 1): 1, (1, 2): 2, (2, 3): 3, (3, 0): 4 }
-  G = Grafo(V, E, w)
+  # G = Grafo(V, E, w)
   # print(G)
   # print(G.qtdVertices())
   # print(G.qtdArestas())
@@ -13,11 +13,12 @@ def main():
   # print(G.vizinhos(0))
   # print(G.haAresta(1, 0))
   # print(G.haAresta(0, 2))
-  print(G.peso(1, 0))
-  print(G.peso(0, 2))
-  print(G.peso(0, 3))
-  G2 = Grafo()
+  # print(G.peso(1, 0))
+  # print(G.peso(0, 2))
+  # print(G.peso(0, 3))
+  # G2 = Grafo()
   # print(G2.ler("facebook_santiago.net"))
+  # print(G2.vizinhos(1))
   
 if __name__ == "__main__":
   main()
